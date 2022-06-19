fecc560339848da59229e1aebb290240d1c56fc8
Sunil Tyagi <styagi@simplifyvms.com>
2022-06-19 20:51:04 +0530

    task-4

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -35,9 +35,81 @@ class CreateCinemaSchema extends Migration
      * As a user I want to know where I'm sitting on my ticket
      * As a cinema owner I dont want to configure the seating for every show
      */
+
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        try {
+            Schema::create('movies', function ($table) {
+                $table->increments('id');
+                $table->string('title');
+                $table->string('runtime');
+                $table->string('release_date');
+                $table->timestamps();
+            });
+
+            Schema::create('cities', function ($table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->timestamps();
+            });
+
+            Schema::create('shows', function ($table) {
+                $table->increments('id');
+                $table->date('date');
+                $table->dateTime('start_time');
+                $table->dateTime('end_time');
+                $table->foreignId('city_id')->references('id')->on('cities');  
+                $table->foreignId('movie_id')->references('id')->on('movies');
+                $table->timestamps();
+            });
+            
+            Schema::create('cinema', function ($table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('total_seat');
+                $table->timestamps();
+            });
+
+            Schema::create('bookings', function ($table) {
+                $table->increments('id');
+                $table->integer('number_of_sheets');
+                $table->foreignId('user_id')->references('id')->on('users');
+                $table->foreignId('show_id')->references('id')->on('shows');
+                $table->integer('status');
+                $table->timestamps();
+            });
+
+
+            Schema::create('cinema_seat', function ($table) {
+                $table->increments('id');
+                $table->string('seat_number');
+                $table->string('type');
+                $table->foreignId('cinema_id')->references('id')->on('cinema');
+                $table->timestamps();
+            });
+            Schema::create('show_seat', function ($table) {
+                $table->increments('id');
+                $table->string('status');
+                $table->string('price');
+                $table->foreignId('cinema_seat_id')->references('id')->on('cinema_seat');
+                $table->foreignId('show_id')->references('id')->on('shows');
+                $table->foreignId('booking_id')->references('id')->on('bookings');
+                $table->timestamps();
+            });
+            Schema::create('payment', function ($table) {
+                $table->increments('id');
+                $table->foreignId('booking_id')->references('id')->on('bookings');
+                $table->string('amount');
+                $table->string('discount_coupon_id');
+                $table->string('transaction_id');
+                $table->string('payment_method');
+                $table->timestamps();
+            });
+            
+        } catch (\Throwable $th) {
+            throw ($th);
+            // throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        }
     }
 
     /**
